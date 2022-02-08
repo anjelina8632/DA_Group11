24fd5fa53892518fb874d924ab5bdeaddce3a935
anjelina8632 <93510894+anjelina8632@users.noreply.github.com>
2022-02-08 15:11:14 +0800

    Update Group11.py

--- Group11.py
+++ Group11.py
@@ -1,13 +1,14 @@
 import scrapy
 
-class NewSpider(scrapy.Spider)
+class NewSpider(scrapy.Spider):
     name = "new spider"
     start_urls = ["https://brickset.com/sets/year-2020"]
 
     def parse(self, response):
-        xpath_selector = "//img"
-        for x in response.xpath(xpath_selector):
+        css_selector = "//img"
+        for x in response.css(css_selector):
             newsel = "@src"
             yield {
                 'Image Link': x.xpath(newsel).extract_first()
-            }b
+            }
+    #K
