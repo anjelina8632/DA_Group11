e999da31d41e85120942944bd0218aea1578649c
anjelina8632 <93510894+anjelina8632@users.noreply.github.com>
2022-02-15 15:13:49 +0800

    Update Group11.py

--- Group11.py
+++ Group11.py
@@ -1,3 +1,25 @@
+#task5
+import requests
+
+url = 'https://brickset.com/sets/year-2020'
+r = requests.get(url)
+
+print(r.text)
+
+print("Source Code:")
+print("\t*", r.status_code)
+
+h = requests.head(url)
+print("Header")
+print("**********")
+
+for x in h.headers:
+    print("\t", x, ".", h.headers[x])
+print("**********")
+
+#task6
+
+#task7
 import scrapy
 
 class NewSpider(scrapy.Spider):
