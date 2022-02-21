6748ef0d8b50fee8b3844f141c98e8ddf21328e0
anjelina8632 <93510894+anjelina8632@users.noreply.github.com>
2022-02-21 15:46:21 +0800

    Update and rename Group11.py to Code_Compile.py

--- Code_Compile.py
+++ Code_Compile.py
@@ -1,4 +1,5 @@
-#task5
+# Compilation of Codes
+#Task 5
 import requests
 
 url = 'https://brickset.com/sets/year-2020'
@@ -27,9 +28,30 @@ rh = requests.get(url2, headers=header)
 
 print(rh.text)   
 
-#task6
+#Task 6
+import scrapy
 
-#task7
+class NewSpider(scrapy.Spider):
+   name = "new_spider"
+   start_urls = ['http://brickset.com/sets/year-2020']
+   def parse(self, response):
+      #xpath_selector = '//img'
+      css_selector='img'
+      for  x in response.xpath(css_selector):
+         newsel = '@src'
+         yield {
+            'Image Link': x.xpath(newsel).extract_first(),
+         }
+#To recurse next page
+      Page_selector = '.next a ::attr(href)'
+      next_page = response.css(Page_selector).extract_first()
+      if next_page:
+         yield scrapy.Request(
+             response.urljoin(next_page),
+             callback=self.parse
+         )
+            
+#Task 7
 import scrapy
 
 class NewSpider(scrapy.Spider):
