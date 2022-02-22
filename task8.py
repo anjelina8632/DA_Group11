e41be2b18ad2ac62280b15fd0c833ac3a2be6645
danielashraff <99797610+danielashraff@users.noreply.github.com>
2022-02-22 15:35:37 +0800

    Add files via upload
    task 8

---
+++ task8.py
@@ -0,0 +1,9 @@
+import unittest # Importing the function to test file
+
+class Task6_and_7(unittest.TestCase): #Inputting the test file
+     def test_EngineType (self) :
+        print('Testing')
+
+# To test the fike
+if __name__ == '__main__' :
+    unittest.main()
