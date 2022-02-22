0b05dc36f5dbb4667451a9ec87e6194a3a040e1e
anjelina8632 <93510894+anjelina8632@users.noreply.github.com>
2022-02-22 13:13:10 +0800

    Update task 5.py

--- task 5.py
+++ task 5.py
@@ -1,13 +1,17 @@
+# Making a request to a web browser
 import requests
 
 url = 'https://brickset.com/sets/year-2020'
 r = requests.get(url)
 
+# Getting the full page of the web browser/site
 print(r.text)
 
+# Getting status code > * 200 - which means OK
 print("Source Code:")
 print("\t*", r.status_code)
 
+# Header response from the web server
 h = requests.head(url)
 print("Header")
 print("**********")
@@ -16,7 +20,7 @@ for x in h.headers:
     print("\t", x, ".", h.headers[x])
 print("**********")
 
-
+# Modifying the header's user agent
 header = {
     'User-Agent' : 'Mobile'
 }
